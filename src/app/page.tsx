cbc86a8ba19d55467fe20fbe611af5c1858f4db2
Yolanda Turner <yolandaturner@Yolandas-MacBook-Pro.local>
2025-07-13 12:15:04 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-rose-500">Hello World</p>
+    </div>
+  )
+}
